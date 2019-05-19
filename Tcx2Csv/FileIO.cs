626f1f873411dae50d2bf89b2e931404ae885539
Joseph Knowles <joseph@josephknowles.com>
2019-05-19 14:32:29 +0100

    Initial cut of conversion

---
+++ Tcx2Csv/FileIO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tcx2Csv
+{
+    using System.IO;
+    using System.Xml.Linq;
+
+    internal class FileIO
+    {
+        internal XDocument ReadXDocumentFromFile(string fileName)
+        {
+            return XDocument.Load(fileName);
+        }
+
+        internal void SaveTextToFile(string textToSave, string fileName)
+        {
+            File.WriteAllText(fileName, textToSave);
+        }
+    }
+}
--- Tcx2Csv/Program.cs
+++ Tcx2Csv/Program.cs
@@ -2,11 +2,32 @@
 
 namespace Tcx2Csv
 {
+    using System.IO;
+    using System.Xml.Linq;
+
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args.Length == 1 && args[0].ToLower().EndsWith(".tcx") &&  File.Exists(args[0]))
+            {
+                var fileIo = new FileIO();
+                var tcx = fileIo.ReadXDocumentFromFile(args[0]);
+                var converter = new Tcx2CsvConverter();
+                var output = converter.ConvertTcx2Csv(tcx);
+                fileIo.SaveTextToFile(output, args[0].Substring(0, args[0].Length - 4) + ".csv");
+
+                return 0;
+            }
+            else
+            {
+                Console.WriteLine("Usage: Enter the filename of the tcx file to be converted and a csv file will be created using the same filename. If you have entered a filename it may not exist.");
+                return -1;
+            }
+
+
         }
+
+        
     }
 }
