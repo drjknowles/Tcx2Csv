180d218263924fa4b3aff6091e3835fa0e506a13
Joseph Knowles <joseph@josephknowles.com>
2019-05-19 13:56:13 +0100

    Initial commit of empty solution

---
+++ Tcx2Csv/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tcx2Csv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
