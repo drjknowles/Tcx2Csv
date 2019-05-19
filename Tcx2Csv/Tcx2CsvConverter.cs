c856ebe82284dd0da612824a8b2b6b3d34ad61e8
Joseph Knowles <joseph@josephknowles.com>
2019-05-19 14:36:15 +0100

    Change of DateTime format

--- Tcx2Csv/Tcx2CsvConverter.cs
+++ Tcx2Csv/Tcx2CsvConverter.cs
@@ -5,6 +5,7 @@ using System.Text;
 namespace Tcx2Csv
 {
     using System.Linq;
+    using System.Reflection.PortableExecutable;
     using System.Xml.Linq;
 
     internal class Tcx2CsvConverter
@@ -22,13 +23,14 @@ namespace Tcx2Csv
             foreach (var trackPoint in trackPoints)
             {
                 string time = trackPoint.Element(xmlNamespace + "Time").Value;
+                DateTime formatTime = DateTime.Parse(time);
                 string latitude = trackPoint.Element(xmlNamespace + "Position").Element(xmlNamespace + "LatitudeDegrees").Value;
                 string longitude = trackPoint.Element(xmlNamespace + "Position").Element(xmlNamespace + "LongitudeDegrees").Value;
                 string altitude = trackPoint.Element(xmlNamespace + "AltitudeMeters").Value;
                 string distance = trackPoint.Element(xmlNamespace + "DistanceMeters").Value;
                 string heartRate = trackPoint.Element(xmlNamespace + "HeartRateBpm").Element(xmlNamespace + "Value").Value;
 
-                csvFile.AppendLine($"{time},{latitude},{longitude},{altitude},{distance},{heartRate}");
+                csvFile.AppendLine($"{formatTime:yy-MM-dd HH:mm:ss},{latitude},{longitude},{altitude},{distance},{heartRate}");
             }
 
             return csvFile.ToString();
